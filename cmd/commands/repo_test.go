765d1a98cebb6665cffd991b95b33a75c48213b8
roi.kramer <roi.kramer@codefresh.io>
2021-04-19 00:36:10 +0300

    added tests to repo create command

---
+++ cmd/commands/repo_test.go
@@ -0,0 +1,30 @@
+package commands
+
+import (
+	"context"
+	"testing"
+
+	"github.com/argoproj/argocd-autopilot/pkg/git"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRunRepoCreate(t *testing.T) {
+	tests := map[string]struct {
+		opts     *RepoCreateOptions
+		assertFn func(t *testing.T, opts *RepoCreateOptions, ret error)
+	}{
+		"Invalid provider": {
+			opts: &RepoCreateOptions{
+				Provider: "foobar",
+			},
+			assertFn: func(t *testing.T, opts *RepoCreateOptions, ret error) {
+				assert.ErrorIs(t, ret, git.ErrProviderNotSupported)
+			},
+		},
+	}
+	for tname, tt := range tests {
+		t.Run(tname, func(t *testing.T) {
+			tt.assertFn(t, tt.opts, RunRepoCreate(context.Background(), tt.opts))
+		})
+	}
+}
