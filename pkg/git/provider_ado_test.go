bcf5c4511b2df96c32d8b5b99aef0049bf15fece
rumstead <rjumstead@gmail.com>
2021-12-21 19:48:05 -0500

    adding another test

--- pkg/git/provider_ado_test.go
+++ pkg/git/provider_ado_test.go
@@ -92,3 +92,41 @@ func Test_adoGit_CreateRepository(t *testing.T) {
 		})
 	}
 }
+
+func Test_parseAdoUrl(t *testing.T) {
+	type args struct {
+		host string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    *adoGitUrl
+		wantErr assert.ErrorAssertionFunc
+	}{
+		{name: "Invalid URL", args: args{host: "https://dev.azure.com"}, want: nil, wantErr: func(t assert.TestingT, err error, i ...interface{}) bool {
+			return true
+		}},
+		// url taking from the url_test in the url/net module
+		{name: "Parse Error", args: args{host: "http://[fe80::%31]:8080/"}, want: nil, wantErr: func(t assert.TestingT, err error, i ...interface{}) bool {
+			return true
+		}},
+		{name: "Parse URL", args: args{host: "https://dev.azure.com/SUB/PROJECT/_git/REPO "}, want: &adoGitUrl{
+			loginUrl:     "https://dev.azure.com/SUB",
+			subscription: "SUB",
+			projectName:  "PROJECT",
+			repoName:     "REPO",
+		},
+			wantErr: func(t assert.TestingT, err error, i ...interface{}) bool {
+				return false
+			}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := parseAdoUrl(tt.args.host)
+			if !tt.wantErr(t, err, fmt.Sprintf("parseAdoUrl(%v)", tt.args.host)) {
+				return
+			}
+			assert.Equalf(t, tt.want, got, "parseAdoUrl(%v)", tt.args.host)
+		})
+	}
+}
