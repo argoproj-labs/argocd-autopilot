b217c50a06ebb146082e630ab4af7a8eaaa5b801
Noam Gal <noam.gal@codefresh.io>
2023-06-04 14:43:34 +0300

    testing stuff
    Signed-off-by: Noam Gal <noam.gal@codefresh.io>

--- hack/test.sh
+++ hack/test.sh
@@ -3,11 +3,15 @@
 set -e
 echo "" > coverage.txt
 
-for d in $(go list ./... | grep -v vendor); do
-    go test -v -race -coverprofile=profile.out -covermode=atomic $d
-    if [ -f profile.out ]; then
-        cat profile.out >> coverage.txt
-        rm profile.out
-    fi
-done
+echo "before github.com/argoproj-labs/argocd-autopilot/hack/cmd-docs"
+go test -v -race -coverprofile=profile.out -covermode=atomic github.com/argoproj-labs/argocd-autopilot/hack/cmd-docs
+echo "after github.com/argoproj-labs/argocd-autopilot/hack/cmd-docs"
+
+# for d in $(go list ./... | grep -v vendor); do
+#     go test -v -race -coverprofile=profile.out -covermode=atomic $d
+#     if [ -f profile.out ]; then
+#         cat profile.out >> coverage.txt
+#         rm profile.out
+#     fi
+# done
 
