9311609045a2ae178dde3c9085388e3d82cae71d
Noam Gal <noam.gal@codefresh.io>
2023-06-15 17:26:15 +0300

    testing stuff
    Signed-off-by: Noam Gal <noam.gal@codefresh.io>

--- hack/test.sh
+++ hack/test.sh
@@ -5,8 +5,8 @@ echo "" > coverage.txt
 
 for d in $(go list ./... | grep -v vendor); do
     echo "tidying"
-    go mod tidy
-    git status
+    # go mod tidy
+    # git status
     echo "before $d"
     go test -v -race -coverprofile=profile.out -covermode=atomic $d
     echo "after $d"
