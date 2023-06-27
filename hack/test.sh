a69d2f692c5a7add1dac5b7139a64ffe31b3788e
Noam Gal <noam.gal@codefresh.io>
2023-06-27 09:00:40 +0300

    testing stuff
    Signed-off-by: Noam Gal <noam.gal@codefresh.io>

--- hack/test.sh
+++ hack/test.sh
@@ -3,13 +3,15 @@
 set -e
 echo "" > coverage.txt
 
+go test -v ./...
+
 for d in $(go list ./...); do
     echo "tidying"
     # go mod tidy
     # git status
     echo "before $d"
     # go test -v -race -coverprofile=profile.out -covermode=atomic $d
-    go test -v $d
+    # go test -v $d
     echo "after $d"
     # if [ -f profile.out ]; then
     #     cat profile.out >> coverage.txt
