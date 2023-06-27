7128e1576e5df0e9fd7d235c14bf5fc6b83df322
Noam Gal <noam.gal@codefresh.io>
2023-06-27 11:17:39 +0300

    removed commented code
    Signed-off-by: Noam Gal <noam.gal@codefresh.io>

--- hack/test.sh
+++ hack/test.sh
@@ -4,17 +4,3 @@ set -e
 echo "" > coverage.txt
 
 go test -v -race -coverprofile=coverage.txt -covermode=atomic ./...
-
-# for d in $(go list ./...); do
-    # echo "tidying"
-    # go mod tidy
-    # git status
-    # echo "before $d"
-    # go test -v -race -coverprofile=profile.out -covermode=atomic $d
-    # go test -v $d
-    # echo "after $d"
-    # if [ -f profile.out ]; then
-    #     cat profile.out >> coverage.txt
-    #     rm profile.out
-    # fi
-# done
